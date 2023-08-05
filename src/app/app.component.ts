e67e60d1ffb010d5460fb989f85c276ffd7b3e35
Imweeraya <weerayakosaldilok@gmail.com>
2023-08-05 15:58:55 +0700

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { TodoList } from './todo-list';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
