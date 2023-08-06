37decdd88f87e5a2815f111cd04d8ce0d6a23452
Imweeraya <weerayakosaldilok@gmail.com>
2023-08-06 11:29:50 +0700

    todo App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,33 @@ import { TodoList } from './todo-list';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private list = new TodoList('Bob', [
+  private list = new TodoList('Bob', [  //create obj froom TodoList
     new TodoItem('Go for run', true),
     new TodoItem('Get flowers'),
     new TodoItem('Collect tickets'),
   ]);
+
+  showComplete = false;
   
   
-  get username(): string {
+  get username(): string { //ใช้ดึงค่าชื่อของ user
     return this.list.user;
   }
     
-  get itemCount(): number {
-    return this.list.items.filter((item) => !item.complete).length;
+  get itemCount(): number { //ใช้ดึงค่าจำนวน todo
+    return this.list.items.filter((item) => !item.complete).length; //filter todo ที่ยังไม่ complete
   }
-  
+
+  get items(): readonly TodoItem[]{ //return เป็น Todoitem
+    // return this.list.items;
+    //return this.list.items.filter((item) => !item.complete); //filter todo ที่ยังไม่ complete
+    return this.list.items.filter(item => this.showComplete || !item.complete); //false || false  // true || false ขึ้นกับ showComplete
+  }
+
+  addmoreItem(newItem: string) {
+    if (newItem != '') {
+      this.list.addItem(newItem); //เรียก method addItem ใน list
+    }
+  }
+
 }
--- src/app/todo-list.ts
+++ src/app/todo-list.ts
@@ -2,12 +2,13 @@ import { TodoItem } from './todo-item';
 export class TodoList {
     // public user: string
     // private todoItems: TodoItem[] = []
-  constructor(public user: string, private todoItems: TodoItem[] = []) { //ประกาศ type ใน parameter ได้เลย
+  constructor(public user: string, private todoItems: TodoItem[] = []) { //ประกาศ property type ใน parameter ได้เลย
     // no statements required
   }
-  get items(): readonly TodoItem[] { //getter
-    return this.todoItems;
+  get items(): readonly TodoItem[] { //getter define เป็น method แต่ข้างนอกเรียกใช้เหมือนเป็นตัวแปร(property) เช่น list.items;
+    return this.todoItems; //เป็น feture ของ Java script ES6
   }
+  
   addItem(task: string) {
     this.todoItems.push(new TodoItem(task));
   }
